6b7d36d7db6661d3791967672b3c77f7881a6703
Christian1091 <christianespinoza935@gmail.com>
2021-09-15 13:41:06 -0500

    feat: change invest for loanflow

--- src/test/java/steps/pom/tests/loanFlow.java
+++ src/test/java/steps/pom/tests/loanFlow.java
@@ -8,9 +8,9 @@ import steps.pom.pages.transactions.Tr062100;
 import java.io.IOException;
 
 
-public class loadFlow extends StartPages {
+public class loanFlow extends StartPages {
 
-    public loadFlow() throws IOException { }
+    public loanFlow() throws IOException { }
 
     public void openBrowser() throws Throwable {
         caseScreen++;
@@ -39,12 +39,16 @@ public class loadFlow extends StartPages {
 
     public void typeTransaction() throws Throwable {
         System.out.println("---" + util.readExcel().get(1));
-        driver.findElement(tr062100.getTxtTransactions()).sendKeys(loanFlow.get(2));
+        driver.findElement(tr062100.getTxtTransactions()).sendKeys(loanFlow.get(2) + Keys.ENTER);
         Thread.sleep(5000);
     }
 
     public void idDeudor() throws Throwable {
-        //util.waitPass(2000, "TR062100");
+        util.waitPass(2000, "TR062100");
         driver.findElement(tr062100.getTxtId()).sendKeys(loanFlow.get(3));
+    }
+    public void idCod() throws Throwable {
+        util.waitPass(2000, "TR062100");
+        driver.findElement(tr062100.getTxtIdCod()).sendKeys(loanFlow.get(4));
     }
 }
