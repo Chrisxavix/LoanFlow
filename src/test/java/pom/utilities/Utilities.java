15fc60441b3b896454057461eae525b8a67e0345
Christian1091 <christianespinoza935@gmail.com>
2021-09-16 16:14:27 -0500

    feat: removed spaces

--- src/test/java/pom/utilities/Utilities.java
+++ src/test/java/pom/utilities/Utilities.java
@@ -40,7 +40,7 @@ public class Utilities {
             cont++;
             Thread.sleep(200);
             status = driver.findElement(global.getTxtStatus()).getText();
-           /*System.out.println("STATUS DEL WHILE: " + status);
+            /*System.out.println("STATUS DEL WHILE: " + status);
             System.out.println("ENTRA AL WHILE: " + cont);*/
         }
         System.out.println("FINAL STATUS: " + status);
